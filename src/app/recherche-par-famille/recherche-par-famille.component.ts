bc71268596ad1417ffce1ae52aed22a2ad5ecef0
Hanyn7 <hanin8100@gmail.com>
2023-10-30 23:29:28 +0100

    Description of the changes you made

--- src/app/recherche-par-famille/recherche-par-famille.component.ts
+++ src/app/recherche-par-famille/recherche-par-famille.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Parfum } from '../model/parfum.model';
 import { Famille } from '../model/famille.model';
 import { ParfumService } from '../services/parfum.service';
+import {  Router } from '@angular/router';
 
 
 @Component({
@@ -11,19 +12,59 @@ import { ParfumService } from '../services/parfum.service';
 export class RechercheParFamilleComponent {
   IdFamille! : number;
   familles! : Famille[];
+  currentParfum = new Parfum();
+  updatedFamId! : number;
   parfums! : Parfum[];
-  constructor(private parfumService : ParfumService) { }
+  noPerfumesFound: boolean = false;
 
-  
+  constructor(private parfumService : ParfumService, private router :Router) { }
+
+  apiURL: string = 'http://localhost:8080/parfums/api';
+
   ngOnInit(): void {
+    this.chargerParfums();
     this.parfumService.listeFamilles().
     subscribe(fams => {this.familles = fams._embedded.familles;
     console.log(fams);
     });
     }
-
-    onChange() {
-      this.parfumService.rechercherParFamille(this.IdFamille).
-      subscribe(parfs =>{this.parfums=parfs});
+    chargerParfums(){
+      this.parfumService.listeParfum().subscribe(parfs => {
+      this.parfums = parfs;
+      });
       }
+      
+      onChange() {
+        this.parfumService.rechercherParFamille(this.IdFamille).subscribe((parfs) => {
+          this.parfums = parfs;
+          
+          if (this.parfums.length === 0) {
+            
+            this.noPerfumesFound = true;
+          } else {
+            this.noPerfumesFound = false;
+          }
+        });
+      }
+      
+      updateParfum() {
+        this.currentParfum.famille = this.familles.find(fam => fam.idFam ==
+        this.updatedFamId)!;
+        this.parfumService
+        .updateParfum(this.currentParfum)
+        .subscribe((parf) => {
+          this.router.navigate(['/RechercheParFamille']);
+        });
+        }
+
+        supprimerParfum(p: Parfum)
+      {
+      let conf = confirm("Etes-vous sûr ?");
+      if (conf)
+      this.parfumService.supprimerParfum(p.idParfum).subscribe(() => {
+      console.log("Parfum supprimé");
+      this.chargerParfums();
+      });
+      }
+        
 }
--- src/app/recherche-par-nom/recherche-par-nom.component.ts
+++ src/app/recherche-par-nom/recherche-par-nom.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Parfum } from '../model/parfum.model';
 import { ParfumService } from '../services/parfum.service';
+import {  Router } from '@angular/router';
+import { Famille } from '../model/famille.model';
 
 @Component({
   selector: 'app-recherche-par-nom',
@@ -9,18 +11,41 @@ import { ParfumService } from '../services/parfum.service';
 export class RechercheParNomComponent implements OnInit {
   nomParfum!: string;
   parfums!: Parfum[];
+  familles! : Famille[];
+  IdFamille! : number;
+  noPerfumesFound: boolean = false;
   allParfums!: Parfum[];
   searchTerm!: string;
+  currentParfum = new Parfum();
+  updatedFamId! : number;
 
-  constructor(private parfumService: ParfumService) { }
+  constructor(private parfumService: ParfumService, private router :Router) { }
 
   ngOnInit(): void {
     this.parfumService.listeParfum().subscribe(parfs => {
       console.log(parfs);
+      this.chargerParfums();
       this.parfums = parfs;
     });
   }
-
+      
+  onChange() {
+    this.parfumService.rechercherParFamille(this.IdFamille).subscribe((parfs) => {
+      this.parfums = parfs;
+      
+      if (this.parfums.length === 0) {
+        
+        this.noPerfumesFound = true;
+      } else {
+        this.noPerfumesFound = false;
+      }
+    });
+  }
+  chargerParfums(){
+    this.parfumService.listeParfum().subscribe(parfs => {
+    this.parfums = parfs;
+    });
+    }
   onKeyUp(filterText : string){
     this.parfums = this.allParfums.filter(item =>
     item.nomParfum.toLowerCase().includes(filterText));
@@ -32,5 +57,24 @@ export class RechercheParNomComponent implements OnInit {
         this.parfums = parfs; 
         console.log(parfs);
       });
+  }
+  updateParfum() {
+    this.currentParfum.famille = this.familles.find(fam => fam.idFam ==
+    this.updatedFamId)!;
+    this.parfumService
+    .updateParfum(this.currentParfum)
+    .subscribe((parf) => {
+      this.router.navigate(['/RechercheParFamille']);
+    });
+    }
+
+    supprimerParfum(p: Parfum)
+  {
+  let conf = confirm("Etes-vous sûr ?");
+  if (conf)
+  this.parfumService.supprimerParfum(p.idParfum).subscribe(() => {
+  console.log("Parfum supprimé");
+  this.chargerParfums();
+  });
   }
 }
